56b045137f7e0b8c7848f418e14dfc4fe29a9c73
Reagin Turner <rturnerafteragnes@gmail.com>
2021-10-10 00:43:32 -0400

    Got some of the recipe data to show on the page. Troubleshooting the ingredients display

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,22 +1,55 @@
-
+var displayRecipe = $("#recipedisplay")
+var titleDisplay = $("#titledisplay")
+var imgDisplay = $("#imagedisplay")
+var timeDisplay = $("#timedisplay")
 
 var getRecipeInfo = function(){
-    var day = document.location.search;
-   
-    var recipeImg = day.split('=')[1];
-    var recipeTitle = day.split('=')[1]
-    var recipeCookTime = day.split('=')[i]
-    var recipeIngred = day.split('=')[1]
-    var recipeInstruct = day.split('=')[1]
+    var getRecipes = JSON.parse(localStorage.getItem('searchedRecipes'));
+    var recipeToDisplay = document.location.search;
+    var recipeNum = recipeToDisplay.split('=')[1]
+    recipeToDisplay = getRecipes[recipeNum]
     
-    if (recipeTitle) {
-        var title = $("</p>").textContent = recipeTitle;
-        $("main").append(title)
+    // getting data from local storage
+    var recipeTitle = recipeToDisplay.Recipe.title
+    var recipeCookTime = recipeToDisplay.Recipe.readyInMinutes
+    var recipeImg = recipeToDisplay.Recipe.image
+    var recipeIngred = recipeToDisplay.Recipe.extendedIngredients
+    var recipeInstruct = recipeToDisplay.Recipe.analyzedInstructions
+    console.log(recipeIngred)
+
+    titleDisplay.text(recipeTitle);
+    imgDisplay.attr({"src":recipeImg});
+    timeDisplay.text("Cooking time: " + recipeCookTime + "mins")
+    displayRecipe.append(titleDisplay, imgDisplay, timeDisplay)
+
+    for (var i=0; i<recipeIngred.length; i++){
+        recipeIngredients = recipeIngred[i].original
+        var showIngred = $("<input>").attr({"type":"checkbox"}).css({"display":"inline-block"}).text(recipeIngredients)
+        displayRecipe.append(showIngred);
+        console.log(recipeIngredients)
+    }
+
+
 
 
     
-        // getRepoIssues(repoName);
-    } else {
+    // var recipeIngred = 
+    // var recipeInstruct = 
+    console.log(recipeNum)
+// console.log(localStorage.searchedRecipes)
+    // if (recipeTitle) {
+    //     var title = $("</p>").textContent = recipeTitle;
+    //     $("main").append(title)
+
+    //when displaying the recipe add type of checkbox
+    
+
+    
+    //     // getRepoIssues(repoName);
+    // } 
+    // else {
     //     document.location.replace('./index.html');
     //   }
 };
+
+getRecipeInfo();
