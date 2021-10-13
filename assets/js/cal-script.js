de2ca65563b731ea0a7633ab9b4b3ba0dcdcb8f6
Sistania Bong <sistaniameilina@gmail.com>
2021-10-13 14:25:56 -0400

    add comments and clean up codes

--- assets/js/cal-script.js
+++ assets/js/cal-script.js
@@ -4,51 +4,41 @@ var userNameHeaderEl = $('#userMealPlan');
 var currentDayEl = $('#currentDay');
 
 
-//
-// function displayHeader(){
-//     var recipes = JSON.parse(localStorage.getItem('searchedRecipes'));
-//     var userName = recipes[0].name
-//     userNameHeaderEl.text(userName +"'s Meal Planner").css({"color":"lightslategrey","padding-top": "50px", "font-family": "Caveat", "font-size":"60px"});
-// }
 
-// date display on the header
-setInterval(function(){
-    var rightNow = moment().format('dddd, MMMM Do YYYY');
-    currentDayEl.text(rightNow).css({"color":"lightslategrey", "font-size":"40px","padding-bottom": "30px", "font-family": "Caveat"});
-},0);
-
+// fx that highlights current day on the calendar
 function selectToday(){
-    
     var today = moment().format('dddd');
-    console.log(today.toLowerCase());
-    console.log($(".accordion-title."+today.toLowerCase()).parent());
-    console.log($(".accordion-title."+today.toLowerCase()).siblings());
     $(".accordion-title."+today.toLowerCase()).parent().css({'background':'thistle'});
-
+    // display current day and date on the header
+    setInterval(function(){
+        var rightNow = moment().format('dddd, MMMM Do YYYY');
+        currentDayEl.text(rightNow).css({"color":"lightslategrey", "font-size":"40px","padding-bottom": "30px", "font-family": "Caveat"});
+    },0);
 }
 
-
+// fx that render user's name on the header and saved recipes on the selected day
 function populateCal(){
     // retrieving scores from the local storage
     var recipes = JSON.parse(localStorage.getItem('searchedRecipes'));
-    var userName = recipes[0].name
-    userNameHeaderEl.text(userName +"'s Meal Planner").css({"color":"lightslategrey","padding-top": "50px", "font-family": "Caveat", "font-size":"60px"});
-    var recipes = JSON.parse(localStorage.getItem('searchedRecipes'));
-    console.log(recipes)
-    for (var i=0;i<recipes.length;i++){
-        var recipe = recipes[i];
-        var selectedDay = recipe.day;
-        var recipeTitle = recipe.Recipe.title;
-        var content = $(".accordion-title."+selectedDay.toLowerCase()).siblings();
-        // content.append($('<button>').attr({'class':"recipeBtn",'id': selectedDay+"Btn", 'data-open':"recipeModal"}).text(recipeTitle));
-        content.append($('<button>').attr({'class':"recipeBtn",'id': "btn-"+i, 'data-open':"recipeModal"}).text(recipeTitle));
-        
+    if (recipes){
+        var userName = recipes[0].name
+        userNameHeaderEl.text(userName +"'s Meal Planner").css({"color":"lightslategrey","padding-top": "50px", "font-family": "Caveat", "font-size":"60px"});
+        var recipes = JSON.parse(localStorage.getItem('searchedRecipes'));
+        console.log(recipes)
+        for (var i=0;i<recipes.length;i++){
+            var recipe = recipes[i];
+            var selectedDay = recipe.day;
+            var recipeTitle = recipe.Recipe.title;
+            var content = $(".accordion-title."+selectedDay.toLowerCase()).siblings();
+            content.append($('<button>').attr({'class':"recipeBtn",'id': "btn-"+i, 'data-open':"recipeModal"}).text(recipeTitle));
+            
+        }
     }
 }
 
+// fx that reveal modal with the preview of the recipe when the recipe button is clicked
 function previewRecipe(event){
     var id = event.currentTarget.id;
-    // var day = dayID.split('Btn')[0];
     var whichRecipe = id.split('-')[1];
     var recipeIndex = parseInt(whichRecipe);
     console.log(recipeIndex)
@@ -63,6 +53,7 @@ function previewRecipe(event){
     
 }
 
+// fx that redirects the user to the selected recipe detail page by passing in the index of the selected recipe
 function redirectHandler(event){
     var recipeIndex = event.currentTarget.id;
     location.href = './recipe.html?recipe=' + recipeIndex;
@@ -70,8 +61,7 @@ function redirectHandler(event){
 
 
 
-// displayHeader();
 selectToday();
 populateCal();
-$('.recipeBtn').on('click', previewRecipe);
-$('.startBtn').on('click',redirectHandler);
+$('.recipeBtn').on('click', previewRecipe); //click event listener triggered when the user clicks the recipe button to preview the recipe
+$('.startBtn').on('click',redirectHandler); //click event listener triggered when the user clicks the "Start Cooking" button
--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,46 +1,34 @@
 var valEl = $('#value'); //select input value element
-
 var searchBtnEl = $('#analyzebtn')
-
-
-var APIKey = "8jtm7TlN22DEZthcqa58FegSVmifcY9ldVQ4tx81P1Zi4vwR"; //API key for spoonacular
-// var APIKeyReagin = "17f44d660ddb486fa6457a9e29671fd3";
+var APIKey = "8jtm7TlN22DEZthcqa58FegSVmifcY9ldVQ4tx81P1Zi4vwR"; //API key for Edamame Nutrition API
 
 // fx to get search value and call on searchRecipes() fx to make API call 
 function searchInputHandler(event){
     event.preventDefault();
-
     var value = valEl.val();
-    console.log(value);
-    searchNutrition(value);
+    searchNutrition(value); //passing in search value to fx searchNutrition
     valEl.val("");
-
 }
 
+// fx that make API request to Edamame Nutrition Analysis API
 var searchNutrition = function (value) {
-    console.log('init search')
-  
+    console.log('init search nutrition')
     var queryURL = "https://api.edamam.com/api/nutrition-data?app_id=b2c2e357&app_key=c44b645c4ff77bb33f829e8a4beb2860&nutrition-type=cooking&ingr="+value;
-
     fetch(queryURL)
       .then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
-            console.log(data);
             if(data.totalWeight === 0 && data.calories === 0){
                 $("#errormessage").text("*Please type in a valid unit ex.(1 cup of rice)").css({"color":"red"})
                 $('#food').text("");
                 $('#cal').text("");
                 $('#weight').text("");
             } else {
-                console.log('herererere')
                 $("#errormessage").text("");
                 $('#food').text(value);
                 $('#cal').text(data.calories + " kcal");
                 $('#weight').text(data.totalWeight + " g");
             }
-            // errorMess.empty();
-            // apiError.empty();
           });
         }
       })
@@ -53,6 +41,7 @@ var searchNutrition = function (value) {
 searchBtnEl.on('click',searchInputHandler); //click event handler that calls on searchInputHandler when the submit button is clicked
 
 
+// Selecting all the necessary elements to display the recipe detail info
 var displayRecipe = $("#recipedisplay")
 var titleDisplay = $("#titledisplay")
 var imgDisplay = $("#imagedisplay")
@@ -60,11 +49,12 @@ var timeDisplay = $("#timedisplay")
 var ingredContainer = $("#foringredients")
 var instructContainer = $("#forinstructions")
 
+// fx that renders the recipe detail info on recipe page
 var getRecipeInfo = function(){
     var getRecipes = JSON.parse(localStorage.getItem('searchedRecipes'));
-    var recipeToDisplay = document.location.search;
+    var recipeToDisplay = document.location.search; //grab the recipe index that is passed from calendar.html
     var recipeNum = recipeToDisplay.split('=')[1]
-    recipeToDisplay = getRecipes[recipeNum]
+    recipeToDisplay = getRecipes[recipeNum] //using the recipe index to grab the selected recipe from the local storage
     
     // getting data from local storage
     var recipeTitle = recipeToDisplay.Recipe.title
@@ -73,6 +63,7 @@ var getRecipeInfo = function(){
     var recipeIngred = recipeToDisplay.Recipe.extendedIngredients
     var recipeInstruct = recipeToDisplay.Recipe.analyzedInstructions[0].steps
 
+    // render recipe title, image, and cooking time
     titleDisplay.text(recipeTitle).css({"font-family": "Caveat", "font-size": "40px", "margin-top": "10px"});
     imgDisplay.attr({"src":recipeImg})
     timeDisplay.text("Cooking time: " + recipeCookTime + "mins");
